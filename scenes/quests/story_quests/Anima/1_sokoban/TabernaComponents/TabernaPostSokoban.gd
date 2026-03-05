3a3f048819de2a0ac31201903138aa7ba95d7fc8
JISaldana <juanignaciojobs@gmail.com>
2026-03-05 20:45:56 -0300

    feat: agregar niveles de sokoban y combate

---
+++ scenes/quests/story_quests/Anima/1_sokoban/TabernaComponents/TabernaPostSokoban.gd
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+class_name TabernaPostSokoban
+extends Node2D
+
+signal cinematic_finished
+
+@export var dialogue: DialogueResource = preload("res://scenes/quests/story_quests/Anima/1_sokoban/TabernaComponents/PostSokoban.dialogue")
+
+@export var animation_player: AnimationPlayer
+
+@export_file("*.tscn") var next_scene: String
+
+@export var spawn_point_path: String
+
+func _ready() -> void:
+	if not GameState.intro_dialogue_shown:
+		DialogueManager.show_dialogue_balloon(dialogue, "", [self])
+		await DialogueManager.dialogue_ended
+		cinematic_finished.emit()
+		GameState.intro_dialogue_shown = true
+
+	if next_scene:
+		(
+			SceneSwitcher
+			. change_to_file_with_transition(
+				next_scene,
+				spawn_point_path,
+				Transition.Effect.FADE,
+				Transition.Effect.FADE,
+			)
+		)
+
+#Cambiar la transicion a la escena llamada molino.scne
